ab96d349b6af06b33447cc7d47afd87275a018db
ysy2025 <ysy2025.outlook.com>
2021-08-02 19:29:52 +0800

    algorithm

---
+++ Algorithm/贪婪算法01.py
@@ -0,0 +1,9 @@
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+final_stations = set()
